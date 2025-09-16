1e1c18fae4c7c635fb81b1d423704ba1a4725ff3
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-09-16 16:23:44 +0700

    Add ui mvc springdoc

--- backend/src/main/java/com/nhom4/moviereservation/MoviereservationApplication.java
+++ backend/src/main/java/com/nhom4/moviereservation/MoviereservationApplication.java
@@ -8,6 +8,30 @@ public class MoviereservationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviereservationApplication.class, args);
+		//String url = "http://localhost:8080/";
+		String swaggerUrl = "http://localhost:8080/swagger-ui.html";
+		//System.out.println("Opening browser to: " + url);
+		//openBrowser(url);
+		openBrowser(swaggerUrl);
+	}
+	private static void openBrowser(String url) {
+		try {
+			String os = System.getProperty("os.name").toLowerCase();
+			if (os.contains("win")) {
+				// Sử dụng lệnh rundll32 để mở trình duyệt trên Windows
+				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} else if (os.contains("mac")) {
+				// Sử dụng lệnh open trên macOS
+				Runtime.getRuntime().exec("open " +  url);
+			} else if (os.contains("nix") || os.contains("nux")) {
+				// Sử dụng lệnh xdg-open trên Linux
+				Runtime.getRuntime().exec("xdg-open " + url);
+			} else {
+				System.err.println("Unsupported operating system.");
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to open browser: " + e.getMessage());
+		}
 	}
 
 }
