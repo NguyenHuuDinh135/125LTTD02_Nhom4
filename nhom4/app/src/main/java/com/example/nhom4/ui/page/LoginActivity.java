a88dabc263358952c0684b81c6e6c14f2fb3052c
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-10-05 19:01:11 +0700

    #53 - init home screen

---
+++ nhom4/app/src/main/java/com/example/nhom4/ui/page/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.nhom4.ui.page;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.nhom4.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
