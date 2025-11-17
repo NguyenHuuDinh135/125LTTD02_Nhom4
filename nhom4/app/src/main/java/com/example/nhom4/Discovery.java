99278311e08243efc62180dcfc694384ee2231c7
Nhật Hào <ngoqhao2005@gmail.com>
2025-11-18 00:32:44 +0700

    #101: Add Story_Calendar_Layout

--- nhom4/app/src/main/java/com/example/nhom4/Discovery.java
+++ nhom4/app/src/main/java/com/example/nhom4/Discovery.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Discovery extends AppCompatActivity {
+public class  Discovery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
