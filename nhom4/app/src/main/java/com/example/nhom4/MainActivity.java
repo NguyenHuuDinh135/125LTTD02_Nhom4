a8b7aced61ebe400d0aa7afe8f91d21ca1b33f34
Nhật Hào <ngoqhao2005@gmail.com>
2025-11-19 16:49:11 +0700

    #111: Add Friends-Bottom-Sheet ( cố hết sưc rồi nhưng k làm bo góc được)

--- nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
+++ nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.nhom4;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.nhom4.ui.page.friend.FriendsBottomSheet;
 import com.example.nhom4.ui.page.post.PostAdapter; // Import adapter mới
 
 public class MainActivity extends AppCompatActivity {
@@ -10,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
 
         ViewPager2 viewPager = findViewById(R.id.viewPagerPosts); // ID mới
         PostAdapter adapter = new PostAdapter(this); // Adapter mới
         viewPager.setAdapter(adapter);
+        //Gọi FriendsBottomSheet
+        //new FriendsBottomSheet().show(getSupportFragmentManager(), "sheet");
     }
 }
     
