310494c263b8b78d85e6741c40ade87a9bed8ae0
Nhật Hào <ngoqhao2005@gmail.com>
2025-11-19 16:49:11 +0700

    #111: Resolve conflict

--- nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
+++ nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
@@ -37,5 +37,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void navigateToFeed() {
         viewPagerMain.setCurrentItem(0, true);
+        ViewPager2 viewPager = findViewById(R.id.viewPagerPosts); // ID mới
+        PostAdapter adapter = new PostAdapter(this); // Adapter mới
+        viewPager.setAdapter(adapter);
+        //Gọi FriendsBottomSheet
+        //new FriendsBottomSheet().show(getSupportFragmentManager(), "sheet");
     }
 }
