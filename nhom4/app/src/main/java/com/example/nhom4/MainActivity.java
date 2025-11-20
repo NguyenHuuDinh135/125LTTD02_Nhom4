72c8d34b51718b023227aa6b9e0211e57e7bbcab
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-11-20 19:21:02 +0700

    #119: Load mood
    # Conflicts:
    #	nhom4/app/build.gradle.kts
    #	nhom4/app/src/main/AndroidManifest.xml
    #	nhom4/app/src/main/res/layout/fragment_discovery.xml
    #	nhom4/app/src/main/res/layout/fragment_main.xml
    #	nhom4/app/src/main/res/layout/partial_main_top_bar.xml
    #	nhom4/app/src/main/res/values/strings.xml
    
    # Conflicts:
    #	nhom4/app/src/main/java/com/example/nhom4/MainActivity.java

--- nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
+++ nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.nhom4.ui.page.adapter.MainPagerAdapter;
 import com.example.nhom4.ui.page.friend.FriendsBottomSheet;
 import com.example.nhom4.ui.page.post.PostAdapter; // Import adapter mới
+import com.example.nhom4.ui.adapter.MainPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         viewPagerMain = findViewById(R.id.viewPagerMain);
 
@@ -37,10 +37,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void navigateToFeed() {
         viewPagerMain.setCurrentItem(0, true);
-        ViewPager2 viewPager = findViewById(R.id.viewPagerPosts); // ID mới
-        PostAdapter adapter = new PostAdapter(this); // Adapter mới
-        viewPager.setAdapter(adapter);
-        //Gọi FriendsBottomSheet
-        //new FriendsBottomSheet().show(getSupportFragmentManager(), "sheet");
     }
 }
