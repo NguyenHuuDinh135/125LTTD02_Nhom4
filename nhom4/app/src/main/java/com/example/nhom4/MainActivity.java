f167c1c5093986170a0df7c7bb157b21120cb214
NguyenMTue <23115053122342@sv.ute.udn.vn>
2025-11-20 15:39:31 +0700

    #111: Add Friends Bottom Sheet

--- nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
+++ nhom4/app/src/main/java/com/example/nhom4/MainActivity.java
@@ -8,6 +8,9 @@ import com.example.nhom4.ui.page.friend.FriendsBottomSheet;
 import com.example.nhom4.ui.page.post.PostAdapter; // Import adapter mới
 import com.example.nhom4.ui.page.adapter.MainPagerAdapter;
 
+import com.example.nhom4.ui.page.friend.FriendsBottomSheet;
+import com.example.nhom4.ui.page.post.PostAdapter; // Import adapter mới
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 viewPagerMain;
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
 
 
         viewPagerMain = findViewById(R.id.viewPagerMain);
@@ -38,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void navigateToFeed() {
         viewPagerMain.setCurrentItem(0, true);
+        ViewPager2 viewPager = findViewById(R.id.viewPagerPosts); // ID mới
+        PostAdapter adapter = new PostAdapter(this); // Adapter mới
+        viewPager.setAdapter(adapter);
+        //Gọi FriendsBottomSheet
+        //new FriendsBottomSheet().show(getSupportFragmentManager(), "sheet");
     }
 }
