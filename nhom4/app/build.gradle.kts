474bb290c69a6e7d38d24d9661262179fb380133
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-11-20 22:02:48 +0700

    #119: Done post

--- nhom4/app/build.gradle.kts
+++ nhom4/app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-
+    implementation("com.google.firebase:firebase-storage")
     // CameraX
     implementation("androidx.camera:camera-core:${cameraxVersion}")
     implementation("androidx.camera:camera-camera2:${cameraxVersion}")
