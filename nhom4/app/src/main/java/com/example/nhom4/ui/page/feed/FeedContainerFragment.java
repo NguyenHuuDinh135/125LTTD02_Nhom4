ef08bd242ba135a3ac59c5aace6a03877d3d696c
DESKTOP-R592BJF\NQV-DESKTOP <qvinhkl10@gmail.com>
2025-11-21 11:55:04 +0700

    #113 feat(post): Add item to send message with widget

--- nhom4/app/src/main/java/com/example/nhom4/ui/page/feed/FeedContainerFragment.java
+++ nhom4/app/src/main/java/com/example/nhom4/ui/page/feed/FeedContainerFragment.java
@@ -1,20 +1,34 @@
 package com.example.nhom4.ui.page.feed;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.nhom4.R;
+import com.example.nhom4.data.model.Message;
+import com.example.nhom4.ui.page.chatbox.ChatboxViewModel;
 import com.example.nhom4.ui.page.post.PostAdapter;
+import com.google.android.material.button.MaterialButton;
 
 public class FeedContainerFragment extends Fragment {
 
     private ViewPager2 viewPagerPosts;
+    private EditText etMessage;
+    private LinearLayout reactionButtonContainer;
+    private  MaterialButton btnSend;
+
+    private ChatboxViewModel viewModel;
+
+    private String currentUserId = "njf1b9ZQfRRrYc9SeEXDUp9ZFez1"; // ID của người dùng hiện tại
 
     @Nullable
     @Override
@@ -27,6 +41,9 @@ public class FeedContainerFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         viewPagerPosts = view.findViewById(R.id.viewPagerPosts);
+        etMessage = view.findViewById(R.id.etMessage);
+        reactionButtonContainer = view.findViewById(R.id.emojiContainer);
+        btnSend = view.findViewById(R.id.btnSend);
 
         // --- SỬA ĐỔI TẠI ĐÂY ---
         // Vì PostAdapter hiện tại của bạn đang dùng dữ liệu giả (fake data)
@@ -44,5 +61,38 @@ public class FeedContainerFragment extends Fragment {
                 }
             });
         }
+
+        etMessage.setOnFocusChangeListener((v, hasFocus) -> {
+           if(hasFocus) {
+               // Hiện nút send tin nhắn lên nhé broo
+               reactionButtonContainer.setVisibility(View.GONE);
+               btnSend.setVisibility(View.VISIBLE);
+           } else {
+               // Ngược lại thì chỉ cần ẩn đi thôi broo
+               reactionButtonContainer.setVisibility(View.VISIBLE);
+               btnSend.setVisibility(View.GONE);
+           }
+        });
+
+        btnSend.setOnClickListener(v -> {sendMessage();});
+
+    }
+
+    private void sendMessage() {
+        String messageText = etMessage.getText().toString();
+        if (TextUtils.isEmpty(messageText)) return;
+
+        Message message = new Message(
+                currentUserId,
+                messageText,
+                "widget"
+        );
+
+        String conversationId = "OpGMIA2nZ8cjCMfow41Z"; // test
+        viewModel.init(conversationId);
+
+        viewModel.sendMessage(message);
+
+        etMessage.setText("");
     }
 }
